26c017b0eb77134e69f870e032988d47f71b917c
Alisander Qoshqosh <qalisander@gmail.com>
2022-12-11 22:50:26 +0400

    assembler_iterpreter: add scan test

--- parsers/src/assembler_interpreter.rs
+++ parsers/src/assembler_interpreter.rs
@@ -1,9 +1,11 @@
+//use crate::assembler_interpreter::Instr::{Call, Declr, Div, End, Inc, Lbl, Mov, Msg};
 use itertools::Itertools;
 
 //https://www.codewars.com/kata/58e61f3d8ff24f774400002c/train/rust
 
 // TODO: use InstrType enum
 // TODO: replace string with &str and reference to orginal string
+#[derive(Debug, PartialEq)]
 enum Instr {
     Mov(Reg, Val), // mov x, y - copy y (either an integer or the value of a register) into register x.
     Inc(Reg),      // inc x - increase the content of register x by one.
@@ -12,8 +14,8 @@ enum Instr {
     Sub(Reg, Val), // sub x, y - subtract y (either an integer or the value of a register) from the register x and stores the result in x (i.e. register[x] -= y).
     Mul(Reg, Val), // mul x, y - same with multiply (i.e. register[x] *= y).
     Div(Reg, Val), // div x, y - same with integer division (i.e. register[x] /= y).
-    Label(Lbl), // label: - define a label position (label = identifier + ":", an identifier being a string that does not match any other command). Jump commands and call are aimed to these labels positions in the program.
-    Jmp(Lbl),   // jmp lbl - jumps to the label lbl.
+    Lbl(Lbl), // label: - define a label position (label = identifier + ":", an identifier being a string that does not match any other command). Jump commands and call are aimed to these labels positions in the program.
+    Jmp(Lbl), // jmp lbl - jumps to the label lbl.
     Cmp(Val, Val), // cmp x, y - compares x (either an integer or the value of a register) and y (either an integer or the value of a register). The result is used in the conditional jumps (jne, je, jge, jg, jle and jl)
     Jne(Lbl), // jne lbl - jump to the label lbl if the values of the previous cmp command were not equal.
     Je(Lbl), // je lbl - jump to the label lbl if the values of the previous cmp command were equal.
@@ -26,30 +28,36 @@ enum Instr {
     Msg(Vec<MsgArg>), // msg 'Register: ', x - this instruction stores the output of the program. It may contain text strings (delimited by single quotes) and registers. The number of arguments isn't limited and will vary, depending on the program.
     End, // end - this instruction indicates that the program ends correctly, so the stored output is returned (if the program terminates without this instruction it should return the default output: see below).
 }
+
+#[derive(Debug, PartialEq)]
 enum MsgArg {
     Reg(Reg),
     Str(String),
 }
+
+#[derive(Debug, PartialEq)]
 struct Lbl(String);
+
+#[derive(Debug, PartialEq)]
 struct Reg(String);
 
+#[derive(Debug, PartialEq)]
 enum Val {
     Reg(Reg),
-    Val(i32),
+    Num(i32),
 }
 
 pub struct AssemblerInterpreter {
     instructions: Vec<Instr>,
-    
 }
 
 impl AssemblerInterpreter {
     pub fn interpret(input: &str) -> Option<String> {
         // NOTE: Parse all this lines. Because we have to know functions in advance
-        let instructions = Self::parse(input);
-        
+        let instructions = Self::scan(input);
+
         let interpreter = AssemblerInterpreter { instructions };
-        
+
         interpreter.interpret_instr()
     }
 
@@ -57,23 +65,50 @@ impl AssemblerInterpreter {
         unimplemented!()
     }
 
-    fn parse(input: &str) -> Vec<Instr> {
-        input.lines().map(Self::parse_line).collect_vec()
+    // TODO: Add err output
+    fn scan(input: &str) -> Vec<Instr> {
+        input.lines().map(Self::scan_line).collect_vec()
     }
 
-    fn parse_line(line: &str) -> Instr {
+    fn scan_line(line: &str) -> Instr {
         unimplemented!()
     }
 }
 
-#[cfg(test)]
-pub mod tests {
-    use super::*;
+fn scan_test() {
+    let input = r"
+; My first program
+mov  a, 5
+inc  a
+call function
+msg  '(5+1)/2 = ', a    ; output message
+end
 
-    #[test]
-    fn simple_test() {
-        let simple_programs = &[
-            r"
+function:
+    div  a, 2
+    ret
+";
+    let expected_instructions = vec![
+        Instr::Mov(Reg("a".to_string()), Val::Num(5)),
+        Instr::Inc(Reg("a".to_string())),
+        Instr::Call(Lbl("function".to_string())),
+        Instr::Msg(vec![
+            MsgArg::Str("(5+1)/2 = ".to_string()),
+            MsgArg::Reg(Reg("a".to_string())),
+        ]),
+        Instr::End,
+        Instr::Lbl(Lbl("function".to_string())),
+        Instr::Div(Reg("a".to_string()), Val::Num(2)),
+        Instr::Ret,
+    ];
+    let instructions = AssemblerInterpreter::scan(input);
+    itertools::assert_equal(instructions, expected_instructions)
+}
+
+#[test]
+fn simple_test() {
+    let simple_programs = &[
+        r"
 ; My first program
 mov  a, 5
 inc  a
@@ -85,7 +120,7 @@ function:
     div  a, 2
     ret
 ",
-            r"
+        r"
 mov   a, 5
 mov   b, a
 mov   c, a
@@ -104,7 +139,7 @@ print:
     msg   a, '! = ', c ; output text
     ret
 ",
-            r"
+        r"
 mov   a, 8            ; value
 mov   b, 0            ; next
 mov   c, 0            ; counter
@@ -135,7 +170,7 @@ print:
     msg   'Term ', a, ' of Fibonacci series is: ', b        ; output text
     ret
 ",
-            r"
+        r"
 mov   a, 11           ; value1
 mov   b, 3            ; value2
 call  mod_func
@@ -151,7 +186,7 @@ mod_func:
     sub   d, c
     ret
 ",
-            r"
+        r"
 mov   a, 81         ; value1
 mov   b, 153        ; value2
 call  init
@@ -198,7 +233,7 @@ print:
     msg   'gcd(', a, ', ', b, ') = ', c
     ret
 ",
-            r"
+        r"
 call  func1
 call  print
 end
@@ -213,7 +248,7 @@ func2:
 print:
     msg 'This program should return null'
 ",
-            r"
+        r"
 mov   a, 2            ; value1
 mov   b, 10           ; value2
 mov   c, a            ; temp1
@@ -236,21 +271,20 @@ print:
     msg a, '^', b, ' = ', c
     ret
 ",
-        ];
+    ];
 
-        let expected = &[
-            Some(String::from("(5+1)/2 = 3")),
-            Some(String::from("5! = 120")),
-            Some(String::from("Term 8 of Fibonacci series is: 21")),
-            Some(String::from("mod(11, 3) = 2")),
-            Some(String::from("gcd(81, 153) = 9")),
-            None,
-            Some(String::from("2^10 = 1024")),
-        ];
+    let expected = &[
+        Some(String::from("(5+1)/2 = 3")),
+        Some(String::from("5! = 120")),
+        Some(String::from("Term 8 of Fibonacci series is: 21")),
+        Some(String::from("mod(11, 3) = 2")),
+        Some(String::from("gcd(81, 153) = 9")),
+        None,
+        Some(String::from("2^10 = 1024")),
+    ];
 
-        for (prg, exp) in simple_programs.iter().zip(expected) {
-            let actual = AssemblerInterpreter::interpret(*prg);
-            assert_eq!(actual, *exp);
-        }
+    for (prg, exp) in simple_programs.iter().zip(expected) {
+        let actual = AssemblerInterpreter::interpret(*prg);
+        assert_eq!(actual, *exp);
     }
 }
