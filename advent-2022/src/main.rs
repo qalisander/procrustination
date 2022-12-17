b6331eebf7aba14f24e9337d50aa5c551d76fe34
Alisander Qoshqosh <qalisander@gmail.com>
2022-12-17 16:08:04 +0400

    add git-secret

--- advent-2022/src/main.rs
+++ advent-2022/src/main.rs
@@ -2,6 +2,9 @@ use reqwest::header::COOKIE;
 use reqwest::Client;
 use tokio::fs;
 
+
+// TODO: # create command line tool clap dialog
+// TODO: #q use scrapper to get name of a problem and description https://github.com/causal-agent/scraper
 // TODO: #q create folder if not exist and add problem template and link
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,13 +17,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     // TODO: #q fetch for today if number is not indicated
 
-    let cookie = fs::read_to_string(".cookie").await?;
+    let cookie = fs::read_to_string("cookie").await?;
     let url = format!("https://adventofcode.com/2022/day/{problem_number}/input");
     let res = Client::new().get(url).header(COOKIE, cookie).send().await?;
-    
-    // TODO: #q setup gpg. Add encrypted cookie
+
     fs::write(format!("./src/day_{problem_number}.txt"), res.text().await?).await?;
 
-    // TODO: #q use scrapper to get name of a problem and description https://github.com/causal-agent/scraper
     Ok(())
 }
