cceee9afaea31354036bffb0e87647e58d2e15a6
Alisander Qoshqosh <qalisander@gmail.com>
2022-12-19 12:55:18 +0400

    add cargo.toml template

--- advent-2022/src/main.rs
+++ advent-2022/src/main.rs
@@ -1,18 +1,15 @@
-use std::ops::{Add, Deref};
-use std::path::Path;
 use chrono::prelude::*;
 use reqwest::header::COOKIE;
 use reqwest::{Client, Url};
 use scraper::{Html, Selector};
+use std::path::Path;
 use tokio::fs;
+use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
 
-// TODO: #q use scrapper to get name of a problem and description https://github.com/causal-agent/scraper
-// TODO: #q create folder if not exist and add problem template and link
-// TODO: #q try to use clap here
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: create struct. Associated const
+    // TODO: create struct. Use clap
     let problem_num = get_problem_from_args()
         .or_else(get_problem_from_input)
         .or_else(get_problem_from_date)
@@ -20,7 +17,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cookie = fs::read_to_string("cookie").await?;
 
     let input: Url = format!("https://adventofcode.com/2022/day/{problem_num}/input").parse()?;
-    let input_res = Client::new().get(input).header(COOKIE, &cookie).send().await?;
+    let input_res = Client::new()
+        .get(input)
+        .header(COOKIE, &cookie)
+        .send()
+        .await?;
 
     let dir_path = format!("./src/day_{problem_num}");
     let dir_path = Path::new(&dir_path);
@@ -35,22 +36,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
         fs::write(path, CODE_TMPL.replace(PROBLEM_NAME_TMPL, &problem_name)).await?;
     }
-    let file = file!();
-    dbg!(file);
+
+    let mut file = OpenOptions::new().append(true).open("Cargo.toml").await?;
+    file.write_all(
+        TOML_TMPL
+            .replace(PROBLEM_NAME_TMPL, &problem_name)
+            .replace(PROBLEM_NUM_TMPL, &problem_num.to_string())
+            .as_bytes(),
+    )
+    .await?;
 
     Ok(())
 }
 
-async fn get_problem_name(problem_num: u8, cookie: &str) -> Result<String, Box<dyn std::error::Error>>{
+async fn get_problem_name(
+    problem_num: u8,
+    cookie: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
     let html: Url = format!("https://adventofcode.com/2022/day/{problem_num}").parse()?;
-    let html_res = Client::new().get(html).header(COOKIE, cookie).send().await?;
+    let html_res = Client::new()
+        .get(html)
+        .header(COOKIE, cookie)
+        .send()
+        .await?;
     let document = html_res.text().await?;
     let html = Html::parse_document(&document);
 
     let selector = Selector::parse("h2").unwrap();
     let txt = html.select(&selector).next().unwrap().html();
     let (_, txt) = txt.split_once(":").expect("String ':' is a part of h2 tag");
-    let (txt, _) = txt.split_once("---").expect("String '---' is a part of h2 tag");
+    let (txt, _) = txt
+        .split_once("---")
+        .expect("String '---' is a part of h2 tag");
     Ok(txt.trim().to_lowercase().replace(" ", "_"))
 }
 
@@ -79,16 +96,20 @@ const CODE_TMPL: &str = r##"
 use advent_2022_rs::get_input_str;
 use itertools::Itertools;
 
-pub fn {PROBLEM_NAME}_1(input: &str) -> usize {
+type Ans1 = todo!();
+type Ans2 = todo!();
+
+pub fn {PROBLEM_NAME}_1(input: &str) -> Ans1 {
     let parsed = parse(input);
     todo!("1")
 }
 
-pub fn {PROBLEM_NAME}_2(input: &str) -> usize {
+pub fn {PROBLEM_NAME}_2(input: &str) -> Ans2 {
     let parsed = parse(input);
     todo!("2")
 }
 
+#[derive(Debug)]
 struct Parsed;
 
 fn parse(str: &str) -> Parsed {
@@ -103,17 +124,37 @@ fn main() {
     println!("Part 2: {ans}");
 }
 
-#[test]
-fn {PROBLEM_NAME}_1_test() {
-    let input = r#""#;
-let ans = {PROBLEM_NAME}_1(input);
-todo!("Test 1");
-}
+#[cfg(test)]
+mod tests {
+    use crate::{parse, {PROBLEM_NAME}_1, {PROBLEM_NAME}_2};
 
-#[test]
-fn {PROBLEM_NAME}_2_test() {
-let input = r#""#;
-let ans = {PROBLEM_NAME}_2(input);
-todo!("Test 2")
+    const INPUT: &str = r#""#;
+
+    #[test]
+    fn parse_test() {
+        let parsed = parse(INPUT);
+        dbg!(&parsed);
+    }
+
+    #[test]
+    fn test_1() {
+        let expected = todo!();
+        let ans = {PROBLEM_NAME}_1(INPUT);
+        assert_eq!(ans, expected);
+    }
+
+    #[test]
+    fn test_2() {
+        let expected = todo!();
+        let ans = {PROBLEM_NAME}_2(INPUT);
+        assert_eq!(ans, expected);
+    }
 }
 "##;
+
+const PROBLEM_NUM_TMPL: &str = "{PROBLEM_NUM}";
+const TOML_TMPL: &str = r#"
+[[bin]]
+name = "day_{PROBLEM_NUM}"
+path = "src/day_{PROBLEM_NUM}/{PROBLEM_NAME}.rs"
+"#;
