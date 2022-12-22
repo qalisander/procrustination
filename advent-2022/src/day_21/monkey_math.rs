178644a45b1bc3b1ee88ca3f87bdb927c8748d31
Alisander Qoshqosh <qalisander@gmail.com>
2022-12-23 03:50:05 +0400

    day 21 part 2

--- advent-2022/src/day_21/monkey_math.rs
+++ advent-2022/src/day_21/monkey_math.rs
@@ -1,8 +1,9 @@
 use crate::Val::*;
 use advent_2022_rs::get_input_str;
-use itertools::{Either, Itertools};
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
+use std::mem;
 
 // https://adventofcode.com/2022/day/21
 
@@ -26,30 +27,43 @@ fn calc_rec(var: Var, var_to_val: &HashMap<Var, &Val>) -> Ans {
             left: var0,
             op,
             right: var1,
-        } => calc(calc_rec(var0, var_to_val), op, calc_rec(var1, var_to_val)),
+        } => op.calc(calc_rec(var0, var_to_val), calc_rec(var1, var_to_val)),
         Un(ans) => ans,
-    }
-}
-
-fn calc(val0: Ans, op: Op, val1: Ans) -> Ans {
-    match op {
-        '+' => val0 + val1,
-        '-' => val0 - val1,
-        '*' => val0 * val1,
-        '/' => val0 / val1,
-        op => panic!("Invalid operation '{op}'"),
     }
 }
 
 pub fn monkey_math_2(input: &str) -> Ans2 {
     let mut stmts = parse(input).0;
     let mut var_info: HashMap<Var, VarInfo> = HashMap::new();
-    for (i, stmt) in stmts.iter().enumerate() {
-        if let Bin {left, op, right}  = stmt.rval {
+    for (i, Stmt { lval, rval }) in stmts.iter().enumerate() {
+        if let Bin { left, op, right } = rval {
             var_info.entry(left).or_default().left = Some(i);
             var_info.entry(right).or_default().right = Some(i);
         }
     }
+
+    // root checks for equality. Because root will be equal 0,
+    // '-' sign preserve equation
+    if let Bin { op, .. } = &mut stmts
+        .iter_mut()
+        .find(|stmt| stmt.lval == "root")
+        .expect("'root' should be in stmt")
+        .rval
+    {
+        op.0 = '-';
+    } else {
+        panic!("'root' should be binary")
+    }
+
+    // replace humn assign statement to root: 0
+    let humn_stmt = stmts
+        .iter_mut()
+        .find(|stmt| stmt.lval == "humn")
+        .expect("'humn' should be in stmt");
+    *humn_stmt = Stmt {
+        lval: "root",
+        rval: Un(0),
+    };
 
     revert_stmts_rec("humn", &var_info, &mut stmts);
 
@@ -60,13 +74,18 @@ pub fn monkey_math_2(input: &str) -> Ans2 {
 /// Reference to index in stmt's vec
 /// where var exist
 #[derive(Default)]
-struct VarInfo{
+struct VarInfo {
     left: Option<usize>,
     right: Option<usize>,
 }
 
-fn revert_stmts_rec(var: Var, vars: &HashMap<Var, VarInfo>, stmts: &mut Vec<Stmt>){
-    let var_info = vars.get(var).unwrap_or_else(|| panic!("{var} is in input"));
+fn revert_stmts_rec(var: Var, vars: &HashMap<Var, VarInfo>, stmts: &mut Vec<Stmt>) {
+    if var == "root" {
+        return;
+    }
+    let var_info = vars
+        .get(var)
+        .unwrap_or_else(|| panic!("'{var}' should be in input"));
     if let Some(i) = var_info.left {
         revert_stmts_rec(stmts[i].lval, vars, stmts);
         stmts[i].revert_mut(Arg::Left);
@@ -76,7 +95,6 @@ fn revert_stmts_rec(var: Var, vars: &HashMap<Var, VarInfo>, stmts: &mut Vec<Stmt
         stmts[i].revert_mut(Arg::Right);
     }
 }
-
 
 #[derive(Debug)]
 struct Parsed<'a>(Vec<Stmt<'a>>);
@@ -90,8 +108,9 @@ struct Stmt<'a> {
 impl<'a> Display for Stmt<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self.rval {
-            Bin {left, op, right} => {
-                write!(f, "{}: {} {} {}", self.lval, left, op, right)?; }
+            Bin { left, op, right } => {
+                write!(f, "{}: {} {} {}", self.lval, left, op.0, right)?;
+            }
             Un(num) => {
                 write!(f, "{}: {}", self.lval, num)?;
             }
@@ -100,63 +119,42 @@ impl<'a> Display for Stmt<'a> {
     }
 }
 
-enum Arg{
+enum Arg {
     Left,
     Right,
 }
 
 impl<'a> Stmt<'a> {
-    pub fn revert(&self, new_root: Arg) -> Stmt {
-        match self.rval {
-            Bin { left, op, right } => match new_root {
-                Arg::Left => Stmt {
-                    lval: left,
-                    rval: Bin {
-                        left: self.lval,
-                        op: revert_op(op),
-                        right: right,
-                    },
-                },
+    pub fn revert_mut(&mut self, new_root: Arg) {
+        if let Stmt {
+            lval,
+            rval: Bin { left, op, right },
+        } = self
+        {
+            match new_root {
+                // a: humn / b -> humn: a * b
+                // a: humn * b -> humn: a / b
+                Arg::Left => {
+                    mem::swap(lval, left);
+                    op.revert_mut();
+                }
+                // a: b / humn -> humn: b / a
+                // a: b * humn -> humn: a / b
                 Arg::Right => {
-                    Stmt{
-                        lval: right,
-                        rval: if is_commutative(op){
-                            Bin {
-                                left: self.lval,
-                                op: revert_op(op),
-                                right: left,
-                            }
-                        } else {
-                            Bin {
-                                left: left,
-                                op: revert_op(op),
-                                right: self.lval,
-                            }
-                        }
+                    mem::swap(lval, right);
+                    if op.is_commutative() {
+                        mem::swap(right, left);
+                        op.revert_mut();
                     }
-                },
-            },
-            Un(num) => *self,
+                }
+            }
         }
     }
 
-    pub fn revert_mut(&mut self, new_root: Arg){
-        *self = self.revert(new_root)
-    }
-}
-
-
-fn is_commutative(ch: Op) -> bool{
-    matches!(ch, '+' | '*')
-}
-
-fn revert_op(ch: Op) -> Op{
-    match ch {
-        '-' => '+',
-        '+' => '-',
-        '/' => '*',
-        '*' => '/',
-        op => panic!("Invalid operation '{op}'"),
+    pub fn revert(&self, new_root: Arg) -> Stmt {
+        let mut stmt = *self;
+        stmt.revert_mut(new_root);
+        stmt
     }
 }
 
@@ -172,7 +170,40 @@ enum Val<'a> {
 
 type Var<'a> = &'a str;
 
-type Op = char;
+#[derive(Debug, Copy, Clone)]
+struct Op(char);
+
+impl Op {
+    fn is_commutative(&self) -> bool {
+        matches!(self.0, '+' | '*')
+    }
+
+    fn revert_mut(&mut self) {
+        self.0 = match self.0 {
+            '-' => '+',
+            '+' => '-',
+            '/' => '*',
+            '*' => '/',
+            op => panic!("Invalid operation '{op}'"),
+        };
+    }
+
+    fn revert(&mut self) -> Op{
+        let mut op = self.clone();
+        op.revert_mut();
+        op
+    }
+
+    fn calc(&self, val0: Ans, val1: Ans) -> Ans {
+        match self.0 {
+            '+' => val0 + val1,
+            '-' => val0 - val1,
+            '*' => val0 * val1,
+            '/' => val0 / val1,
+            op => panic!("Invalid operation '{op}'"),
+        }
+    }
+}
 
 fn parse(str: &str) -> Parsed {
     let vec = str
@@ -190,7 +221,7 @@ fn parse(str: &str) -> Parsed {
                     lval: val.strip_suffix(':').expect("Has suffix ':'"),
                     rval: Bin {
                         left,
-                        op: op.parse().expect("Op is char"),
+                        op: Op(op.parse().expect("Op is char")),
                         right,
                     },
                 }
@@ -207,7 +238,7 @@ fn main() {
     let ans = monkey_math_1(&str);
     println!("Part 1: {ans}");
     let ans = monkey_math_2(&str);
-    println!("Part 2: {ans}");
+    println!("Part 2: {ans}"); // 3451534022348
 }
 
 #[cfg(test)]
@@ -245,27 +276,11 @@ hmdt: 32
         assert_eq!(ans, expected);
     }
 
-//    root: pppw(150) + sjmn(150) // root = 0
-//    dbpl: 5
-//    cczh: sllz(4) + lgvd(596) // 600
-//    zczc: 2
-//    ptdq: humn(301) - dvpt(3) // 298
-//    dvpt: 3
-//    lfqf: 4
-//    humn: 5 //301
-//    ljgn: 2
-//    sjmn: drzm(30) * dbpl(5)
-//    sllz: 4
-//    pppw: cczh(600) / lfqf(4) // 150
-//    lgvd: ljgn(2) * ptdq(298) // 596
-//    drzm: hmdt(32) - zczc(2) // 30
-//    hmdt: 32
-
     #[test]
     fn rev_stmt_test() {
         let parsed = parse("pppw: cczh / lfqf").0.into_iter().next().unwrap();
         let reverted = parsed.revert(Arg::Right);
-        assert_eq!(reverted.to_string(), "lfqf: cczh * pppw");
+        assert_eq!(reverted.to_string(), "lfqf: cczh / pppw");
         let reverted = parsed.revert(Arg::Left);
         assert_eq!(reverted.to_string(), "cczh: pppw * lfqf");
 
@@ -275,6 +290,38 @@ hmdt: 32
         let reverted = parsed.revert(Arg::Left);
         assert_eq!(reverted.to_string(), "pppw: root - sjmn");
     }
+
+    //    root: pppw(150) + sjmn(150) // root = 0
+    //    dbpl: 5
+    //    cczh: sllz(4) + lgvd(596) // 600
+    //    zczc: 2
+    //    ptdq: humn(301) - dvpt(3) // 298
+    //    dvpt: 3
+    //    lfqf: 4
+    //    humn: 5 //301 ---
+    //    ljgn: 2
+    //    sjmn: drzm(30) * dbpl(5)
+    //    sllz: 4
+    //    pppw: cczh(600) / lfqf(4) // 150
+    //    lgvd: ljgn(2) * ptdq(298) // 596
+    //    drzm: hmdt(32) - zczc(2) // 30
+    //    hmdt: 32
+
+    //  root: pppw + sjmn
+    //      pppw: cczh / lfqf
+    //          cczh: sllz + lgvd
+    //              sllz: 4
+    //              lgvd: ljgn * ptdq
+    //                  ljgn: 2
+    //                  ptdq: humn - dvpt
+    //                      humn: 5
+    //                      dvpt: 3
+    //          lfqf: 4
+    //      sjmn: drzm * dbpl
+    //          drzm: hmdt - zczc
+    //              hmdt: 32
+    //              zczc: 2
+    //          dbpl: 5
 
     #[test]
     fn test_2() {
