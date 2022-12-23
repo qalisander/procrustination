c4b663001907d751f4f005014392b31f24c88a59
Alisander Qoshqosh <qalisander@gmail.com>
2022-12-23 20:07:35 +0400

    day 3 part 2

--- advent-2022/src/day_3/rucksack_reorganization.rs
+++ advent-2022/src/day_3/rucksack_reorganization.rs
@@ -35,11 +35,6 @@ fn get_score(ch: char) -> Ans1 {
     }
 }
 
-pub fn rucksack_reorganization_2(input: &str) -> Ans2 {
-    let parsed = parse(input);
-    todo!("2")
-}
-
 type Parsed<'a> = Vec<(&'a str, &'a str)>;
 
 fn parse(str: &str) -> Parsed {
@@ -49,6 +44,35 @@ fn parse(str: &str) -> Parsed {
             let split_indx = l.char_indices().nth(count / 2).unwrap().0;
             l.split_at(split_indx)
         })
+        .collect_vec()
+}
+
+pub fn rucksack_reorganization_2(input: &str) -> Ans2 {
+    let parsed = parse2(input);
+    parsed
+        .into_iter()
+        .map(|str| {
+            let set0: HashSet<char> = str.0.chars().collect();
+            let set1: HashSet<char> = str.1.chars().collect();
+            let set2: HashSet<char> = str.2.chars().collect();
+            *set0
+                .intersection(&set1)
+                .cloned()
+                .collect::<HashSet<_>>()
+                .intersection(&set2)
+                .into_iter()
+                .next()
+                .expect("One common char")
+        })
+        .map(get_score)
+        .sum()
+}
+
+type Parsed2<'a> = Vec<(&'a str, &'a str, &'a str)>;
+
+fn parse2(str: &str) -> Parsed2 {
+    str.lines()
+        .batching(|l| l.take(3).collect_tuple::<(_, _, _)>())
         .collect_vec()
 }
 
@@ -80,6 +104,12 @@ CrZsJsPPZsGzwwsLwLmpwMDw
     }
 
     #[test]
+    fn parse2_test() {
+        let parsed = parse2(INPUT.trim());
+        dbg!(&parsed);
+    }
+
+    #[test]
     fn test_1() {
         let expected = 157;
         let ans = rucksack_reorganization_1(INPUT.trim());
@@ -88,7 +118,7 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
     #[test]
     fn test_2() {
-        let expected = todo!();
+        let expected = 70;
         let ans = rucksack_reorganization_2(INPUT.trim());
         assert_eq!(ans, expected);
     }
