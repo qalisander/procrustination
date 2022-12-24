5f1e2c2406bb3fec50bcecaa6fe7d804d98d9628
Alisander Qoshqosh <qalisander@gmail.com>
2022-12-24 18:41:17 +0400

    day 23 part 1

--- advent-2022/src/day_23/unstable_diffusion.rs
+++ advent-2022/src/day_23/unstable_diffusion.rs
@@ -1,10 +1,13 @@
 use advent_2022_rs::get_input_str;
 use derive_more::Add;
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 use std::collections::{HashMap, HashSet};
+use std::fmt::{Display, Formatter};
 use std::ops::Index;
 
 // https://adventofcode.com/2022/day/23
+// outside your scan, more empty ground extends a long way in every direction.
+// If no other Elves are in one of those eight positions, the Elf does not do anything during this round.
 // If there is no Elf in the N, NE, or NW adjacent positions, the Elf proposes moving north one step.
 // If there is no Elf in the S, SE, or SW adjacent positions, the Elf proposes moving south one step.
 // If there is no Elf in the W, NW, or SW adjacent positions, the Elf proposes moving west one step.
@@ -15,18 +18,22 @@ type Ans1 = i32;
 type Ans2 = i32;
 
 pub fn unstable_diffusion_1(input: &str) -> Ans1 {
-    let mut elfs = parse(input).elfs;
+    let mut elfs = parse(input);
     let mut dirs = Dir::new();
 
     for _ in 0..10 {
         let mut new_to_prev: HashMap<Coord, Vec<Coord>> =
-            elfs.iter().cloned().map(|elf| (elf, vec![])).collect();
-        for elf in elfs {
+            elfs.0.iter().cloned().map(|elf| (elf, vec![])).collect();
+        for &elf in &elfs.0 {
+            if Dir::get_all().all(|d| !elfs.0.contains(&(d + elf))) {
+                continue;
+            }
+
             let new_elf = dirs
                 .get_deltas()
                 .filter_map(|delta| {
-                    for adjacent_elf in delta.get_adjacent().map(|d| d + elf) {
-                        if elfs.contains(&adjacent_elf) {
+                    for adjacent_elf in delta.get_adjacent().map(|delta| delta + elf) {
+                        if elfs.0.contains(&adjacent_elf) {
                             return None;
                         }
                     }
@@ -38,9 +45,37 @@ pub fn unstable_diffusion_1(input: &str) -> Ans1 {
                 new_to_prev.remove(&elf);
             }
         }
-        dirs.next_round()
+
+        let to_remove = new_to_prev
+            .iter()
+            .filter(|(k, v)| v.len() > 1)
+            .map(|(k, v)| *k)
+            .collect_vec();
+        for new_elf in to_remove {
+            for prev_elf in new_to_prev.remove(&new_elf).unwrap() {
+                assert!(new_to_prev.insert(prev_elf, vec![]).is_none());
+            }
+        }
+
+        elfs.0 = new_to_prev.into_keys().collect();
+        dirs.next_round();
     }
-    todo!("1")
+
+    let (min_i, max_i) = elfs
+        .0
+        .iter()
+        .map(|elf| elf.0)
+        .minmax()
+        .into_option()
+        .unwrap();
+    let (min_j, max_j) = elfs
+        .0
+        .iter()
+        .map(|elf| elf.1)
+        .minmax()
+        .into_option()
+        .unwrap();
+    (max_i - min_i + 1) * (max_j - min_j + 1) - elfs.0.len() as i32
 }
 
 pub fn unstable_diffusion_2(input: &str) -> Ans2 {
@@ -73,11 +108,43 @@ impl Dir {
             .map(|i| ((i + self.first_indx) % 4) as usize)
             .map(|i| Self::DIRS[i])
     }
+
+    fn get_all() -> impl Iterator<Item = Coord> {
+        [N, S, W, E, N + W, N + E, S + W, S + E].into_iter()
+    }
 }
 
 #[derive(Debug)]
-struct Parsed {
-    elfs: HashSet<Coord>,
+struct Elfs(HashSet<Coord>);
+
+impl Display for Elfs {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (min_i, max_i) = self
+            .0
+            .iter()
+            .map(|elf| elf.0)
+            .minmax()
+            .into_option()
+            .unwrap();
+        let (min_j, max_j) = self
+            .0
+            .iter()
+            .map(|elf| elf.1)
+            .minmax()
+            .into_option()
+            .unwrap();
+        for i in min_i..=max_i {
+            for j in min_j..=max_j {
+                if self.0.contains(&Coord(i, j)) {
+                    write!(f, "#")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 /// Coordinates abstraction
@@ -97,19 +164,19 @@ impl Coord {
     }
 }
 
-fn parse(str: &str) -> Parsed {
-    let elfs: HashSet<_> = str
+fn parse(str: &str) -> Elfs {
+    let elfs = str
         .lines()
         .enumerate()
         .flat_map(|(i, line)| {
             line.char_indices().filter_map(move |(j, ch)| match ch {
                 '.' => None,
-                '#' => Some(Coord(i, j)),
+                '#' => Some(Coord(i as i32, j as i32)),
                 ch => panic!("Unexpected char '{ch}"),
             })
         })
         .collect();
-    Parsed { elfs }
+    Elfs(elfs)
 }
 
 fn main() {
@@ -124,7 +191,7 @@ fn main() {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#"
+    const INPUT_0: &str = r#"
 .....
 ..##.
 ..#..
@@ -133,23 +200,38 @@ mod tests {
 .....
     "#;
 
+    const INPUT_1: &str = r#"
+..............
+..............
+.......#......
+.....###.#....
+...#...#.#....
+....#...##....
+...#.###......
+...##.#.##....
+....#..#......
+..............
+..............
+..............
+    "#;
+
     #[test]
     fn parse_test() {
-        let parsed = parse(INPUT.trim());
+        let parsed = parse(INPUT_1.trim());
         dbg!(&parsed);
     }
 
     #[test]
     fn test_1() {
-        let expected = todo!();
-        let ans = unstable_diffusion_1(INPUT.trim());
+        let expected = 110;
+        let ans = unstable_diffusion_1(INPUT_1.trim());
         assert_eq!(ans, expected);
     }
 
     #[test]
     fn test_2() {
         let expected = todo!();
-        let ans = unstable_diffusion_2(INPUT.trim());
+        let ans = unstable_diffusion_2(INPUT_1.trim());
         assert_eq!(ans, expected);
     }
 }
