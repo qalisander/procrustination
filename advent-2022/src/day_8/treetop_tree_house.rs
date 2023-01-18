32f7e1a9a574860a76e59a7c85191a677228a373
Alisander Qoshqosh <qalisander@gmail.com>
2023-01-18 14:08:34 +0400

    day_8 parse

--- advent-2022/src/day_8/treetop_tree_house.rs
+++ advent-2022/src/day_8/treetop_tree_house.rs
@@ -1,11 +1,10 @@
-
 use advent_2022_rs::get_input_str;
 use itertools::Itertools;
 
 // https://adventofcode.com/2022/day/8
 
-type Ans1 = todo!();
-type Ans2 = todo!();
+type Ans1 = u32;
+type Ans2 = u32;
 
 pub fn treetop_tree_house_1(input: &str) -> Ans1 {
     let parsed = parse(input);
@@ -18,10 +17,14 @@ pub fn treetop_tree_house_2(input: &str) -> Ans2 {
 }
 
 #[derive(Debug)]
-struct Parsed;
+struct Parsed(Vec<Vec<char>>);
 
 fn parse(str: &str) -> Parsed {
-    todo!("Parse")
+    let vec_vec = str
+        .split_whitespace()
+        .map(|str| str.chars().collect())
+        .collect();
+    Parsed(vec_vec)
 }
 
 fn main() {
@@ -37,10 +40,19 @@ mod tests {
     use super::*;
 
     const INPUT: &str = r#"
+30373
+25512
+65332
+33549
+35390
 "#;
 
     fn get_input() -> &'static str {
-        INPUT.strip_prefix('\n').unwrap().strip_suffix('\n').unwrap()
+        INPUT
+            .strip_prefix('\n')
+            .unwrap()
+            .strip_suffix('\n')
+            .unwrap()
     }
 
     #[test]
@@ -51,7 +63,7 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let expected = todo!();
+        let expected = 21;
         let ans = treetop_tree_house_1(get_input());
         assert_eq!(ans, expected);
     }
