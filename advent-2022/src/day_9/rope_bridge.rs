5a432e5c5910aa263d889780094aa0aa95148047
Alisander Qoshqosh <qalisander@gmail.com>
2023-02-02 14:51:24 +0400

    day_9 part 1

--- advent-2022/src/day_9/rope_bridge.rs
+++ advent-2022/src/day_9/rope_bridge.rs
@@ -1,15 +1,32 @@
+use std::collections::HashSet;
 use std::iter;
+use std::ops::Div;
 use advent_2022_rs::get_input_str;
 use itertools::Itertools;
+use derive_more::*;
 
 // https://adventofcode.com/2022/day/9
 
-type Ans1 = u32;
+type Ans1 = usize;
 type Ans2 = u32;
 
 pub fn rope_bridge_1(input: &str) -> Ans1 {
-    let parsed = parse(input);
-    todo!("1")
+    let dirs = parse(input).0;
+    let mut tails = HashSet::new();
+
+    let mut head = Coord(0, 0);
+    let mut tail = Coord(0, 0);
+
+    for dir in dirs {
+        tails.insert(tail);
+        let new_head = head + dir.into();
+        if tail.dist(new_head) > 1 {
+            tail = head;
+        }
+        head = new_head;
+    }
+
+    tails.len()
 }
 
 pub fn rope_bridge_2(input: &str) -> Ans2 {
@@ -24,9 +41,29 @@ struct Parsed(Vec<Dir>);
 #[derive(Debug, Copy, Clone)]
 enum Dir{
     R,
-    U,
     L,
+    U,
     D,
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone, Add)]
+struct Coord(i32, i32);
+
+impl From<Dir> for Coord {
+    fn from(value: Dir) -> Self {
+        match value {
+            Dir::R => Coord(1, 0),
+            Dir::L => Coord(-1, 0),
+            Dir::U => Coord(-1, 0),
+            Dir::D => Coord(1, 0),
+        }
+    }
+}
+
+impl Coord {
+    fn dist(&self, rhs: Coord) -> i32{
+        (self.0 - rhs.0).abs().max((self.1 - rhs.1).abs())
+    }
 }
 
 fn parse(str: &str) -> Parsed {
@@ -80,7 +117,7 @@ R 2
 
     #[test]
     fn test_1() {
-        let expected = todo!();
+        let expected = 13;
         let ans = rope_bridge_1(get_input());
         assert_eq!(ans, expected);
     }
