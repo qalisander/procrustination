4fa5e8a272d9364b9efce19a6e3a476bb8d64992
Alisander Qoshqosh <qalisander@gmail.com>
2023-02-19 17:37:53 +0400

    day_10 parse

--- advent-2022/src/day_10/cathode_ray_tube.rs
+++ advent-2022/src/day_10/cathode_ray_tube.rs
@@ -1,11 +1,12 @@
 
 use advent_2022_rs::get_input_str;
 use itertools::Itertools;
+use crate::Instr::{Addx, Noop};
 
 // https://adventofcode.com/2022/day/10
 
-type Ans1 = todo!();
-type Ans2 = todo!();
+type Ans1 = i32;
+type Ans2 = i32;
 
 pub fn cathode_ray_tube_1(input: &str) -> Ans1 {
     let parsed = parse(input);
@@ -18,10 +19,24 @@ pub fn cathode_ray_tube_2(input: &str) -> Ans2 {
 }
 
 #[derive(Debug)]
-struct Parsed;
+struct Parsed(Vec<Instr>);
+
+#[derive(Debug)]
+enum Instr{
+    Noop,
+    Addx(i32),
+}
 
 fn parse(str: &str) -> Parsed {
-    todo!("Parse")
+    let vec = str.lines().map(|l| {
+        let split = l.split_whitespace().collect_vec();
+        match split.len() {
+            1 if split[0] == "noop" => Noop,
+            2 if split[0] == "addx" => Addx(split[1].parse().expect("Integer parsed")),
+            _ => panic!("Invalid line! '{l}'")
+        }
+    }).collect();
+    Parsed(vec)
 }
 
 fn main() {
@@ -37,6 +52,152 @@ mod tests {
     use super::*;
 
     const INPUT: &str = r#"
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
 "#;
 
     fn get_input() -> &'static str {
