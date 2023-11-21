d13fd5e74e8c926d93273ebfe079724e071738f4
Alisander Qoshqosh <qalisander@gmail.com>
2023-11-21 05:38:20 +0400

    ++

--- leetcode/src/longest_palindromic_substring.rs
+++ leetcode/src/longest_palindromic_substring.rs
@@ -2,35 +2,41 @@
 
 struct Solution;
 
+// l - s.len()
+// runtime: O(l^2)
+// memory: O(l)
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
         let chars: Vec<_> = s.chars().collect();
-        if chars.is_empty() { panic!("Str is empty") }
+        if chars.len() <= 1 {
+            return s;
+        }
 
-        // there is at least single
-        let mut longest: &[char] = &chars[0..1];
-        for start in 0..chars.len() {
-            for end in (start..chars.len()).rev() {
-                let mut i = start;
-                let mut j = end;
-                loop {
-                    if i >= j {
-                        if longest.len() < chars[start..=end].len()
-                        {
-                            longest = &chars[start..=end];
-                        }
-                        break;
-                    } else if chars[i] == chars[j] {
-                        i += 1;
-                        j -= 1;
-                    } else {
-                        break;
-                    }
-                }
+        let mut max_palindrome = &chars[0..1];
+        for i in 0..chars.len() - 1 {
+            let odd = Solution::expand_max_palindrome(&chars, i, i);
+            if max_palindrome.len() < odd.len() {
+                max_palindrome = odd;
+            }
+            let even = Solution::expand_max_palindrome(&chars, i, i + 1);
+            if max_palindrome.len() < even.len() {
+                max_palindrome = even;
             }
         }
 
-        longest.iter().collect()
+        max_palindrome.iter().collect()
+    }
+
+    // cbbd
+    fn expand_max_palindrome(chars: &Vec<char>, left: usize, right: usize) -> &[char] {
+        let len = chars.len() as i32;
+        let mut i = left as i32; // 1
+        let mut j = right as i32; // 2
+        while i >= 0 && j < len && chars[i as usize] == chars[j as usize] {
+            i -= 1;
+            j += 1;
+        }
+        &chars[(i + 1) as usize..j as usize]
     }
 }
 
@@ -50,6 +56,14 @@ mod test {
     fn check2() {
         let input = "cbbd".to_owned();
         let expected = "bb".to_owned();
+        let output = Solution::longest_palindrome(input);
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn check3() {
+        let input = "bannannaas".to_owned();
+        let expected = "annanna".to_owned();
         let output = Solution::longest_palindrome(input);
         assert_eq!(expected, output);
     }
