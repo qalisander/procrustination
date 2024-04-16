36b03804dc26d872d9698ef171e3bd2f15d19e9e
Alisander Qoshqosh <qalisander@gmail.com>
2024-04-16 14:34:16 +0400

    update documentation for stylus_inheritance

--- src/bin/stylus_inheritance.rs
+++ src/bin/stylus_inheritance.rs
@@ -1,22 +1,23 @@
-use crate::oz_lib::base::{Erc721Base, Erc721BaseOverride};
-use crate::oz_lib::pausable::Erc721PausableOverride;
-use crate::oz_lib::Erc721;
-use crate::stylus_lib::TopLevelStorage;
-use std::borrow::{Borrow, BorrowMut};
-
-// NOTE: In this example other extensions won't access their own storage after update override
-
+// aka stylus_sdk
 mod stylus_lib {
+    // Stylus sdk demands this trait to be implemented for
+    // the terminal struct of the smart contract.
     pub trait TopLevelStorage {}
 }
 
+// aka rust_contracts_stylus
 mod oz_lib {
     use crate::stylus_lib::TopLevelStorage;
-    use base::Erc721Base;
-    use pausable::Erc721Pausable;
+    use base::{Erc721Base, Erc721BaseOverride};
+    use pausable::{Erc721Pausable, Erc721PausableOverride};
     use std::borrow::BorrowMut;
-    use std::marker::PhantomData;
 
+    pub type Override = Erc721PausableOverride<Erc721BaseOverride>;
+
+    // Trait used for overriding behaviour of update function.
+    // Other functions can be added to this update function.
+    // Or associated type for every "virtual" method can be used,
+    // that will be restricted with trait named like (Erc721UpdateVirtual).
     pub trait Erc721Virtual {
         fn update<S, T>(storage: &mut S)
         where
@@ -24,11 +25,11 @@ mod oz_lib {
             S: TopLevelStorage + BorrowMut<Erc721<T>>;
     }
 
+    // Library contract that will be reused by our consumers
     #[derive(Debug, Default)]
     pub struct Erc721<T: Erc721Virtual> {
         base: Erc721Base<T>,
         pausable: Erc721Pausable<T>,
-        phantom_data: PhantomData<T>,
     }
 
     pub mod pausable {
@@ -39,12 +40,15 @@ mod oz_lib {
 
         #[derive(Debug, Default)]
         pub struct Erc721Pausable<T: Erc721Virtual> {
+            // Other fields for pausable omitted for simplicity
             phantom_data: PhantomData<T>,
         }
 
+        // Overriding update function for pausalbe extension.
+        // Here we can access Erc721<_> parent storage.
+        // Basically Erc721Pausable and Erc721Base can be mutated.
         #[derive(Debug, Default)]
         pub struct Erc721PausableOverride<T: Erc721Virtual>(PhantomData<T>);
-
         impl<Base: Erc721Virtual> Erc721Virtual for Erc721PausableOverride<Base> {
             fn update<S, This>(storage: &mut S)
             where
@@ -65,10 +69,13 @@ mod oz_lib {
 
         #[derive(Debug, Default)]
         pub struct Erc721Base<T: Erc721Virtual> {
+            // Other fields for erc721 missed for simplicity
             phantom_data: PhantomData<T>,
         }
 
+        // Simplicity sake, we omit #[external] attribute and stylus sdk dependency.
         impl<T: Erc721Virtual> Erc721Base<T> {
+            // Public transfer function of Erc721Base contract.
             pub fn transfer<S>(storage: &mut S)
             where
                 S: TopLevelStorage + BorrowMut<Erc721<T>>,
@@ -78,9 +85,9 @@ mod oz_lib {
             }
         }
 
+        // Base implementation of update function.
         #[derive(Debug, Default)]
         pub struct Erc721BaseOverride;
-
         impl Erc721Virtual for Erc721BaseOverride {
             fn update<S, T>(storage: &mut S)
             where
@@ -93,30 +100,60 @@ mod oz_lib {
     }
 }
 
-// NOTE: client code
+// Client code
+use crate::oz_lib::base::Erc721Base;
+use crate::oz_lib::{Erc721, Erc721Virtual};
+use crate::stylus_lib::TopLevelStorage;
+use std::borrow::{Borrow, BorrowMut};
+use std::marker::PhantomData;
 
-type Override = Erc721PausableOverride<Erc721BaseOverride>;
+type Override = Erc721UserOverride<oz_lib::Override>;
+
+// User can override but won't access storage of his own contract (UserToken)
+// because of constraint of Erc721Virtual trait.
+#[derive(Debug, Default)]
+pub struct Erc721UserOverride<T: Erc721Virtual>(PhantomData<T>);
+impl<Base: Erc721Virtual> Erc721Virtual for Erc721UserOverride<Base> {
+    fn update<S, This>(storage: &mut S)
+    where
+        This: Erc721Virtual,
+        S: TopLevelStorage + BorrowMut<Erc721<This>>,
+    {
+        println!("call user update");
+        Base::update(storage);
+    }
+}
 
 #[derive(Default)]
-struct Token {
+struct UserToken {
+    // Smth else can be at UserToken storage
     erc721: Erc721<Override>,
 }
 
-impl Borrow<Erc721<Override>> for Token {
+impl UserToken {
+    fn user_custom_transfer(&mut self) {
+        Erc721Base::<Override>::transfer(self);
+    }
+}
+
+// UserToken is terminal struct of contract. Then it should be TopLevelStorage.
+impl TopLevelStorage for UserToken {}
+
+// Auto implemented with #[borrow] proc macro from stylus lib.
+impl Borrow<Erc721<Override>> for UserToken {
     fn borrow(&self) -> &Erc721<Override> {
         &self.erc721
     }
 }
 
-impl BorrowMut<Erc721<Override>> for Token {
+// Auto implemented with #[borrow] proc macro from stylus lib.
+impl BorrowMut<Erc721<Override>> for UserToken {
     fn borrow_mut(&mut self) -> &mut Erc721<Override> {
         &mut self.erc721
     }
 }
 
-impl TopLevelStorage for Token {}
-
 fn main() {
-    let mut token = Token::default();
-    Erc721Base::<Override>::transfer(&mut token);
+    let mut token = UserToken::default();
+    token.user_custom_transfer();
 }
