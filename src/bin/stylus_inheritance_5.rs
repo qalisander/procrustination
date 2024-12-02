b95b0451aa661d3fdc67239f974ff86a433df141
Alisander Qoshqosh <qalisander@gmail.com>
2024-12-02 20:26:35 +0400

    ++

--- src/bin/stylus_inheritance_5.rs
+++ src/bin/stylus_inheritance_5.rs
@@ -1,5 +1,6 @@
-use crate::oz_lib::{Erc721, IErc721, IPausable, Pausable};
+use crate::oz_lib::{Erc721, IErc721, IPausable, Pausable, VErc721};
 use crate::stylus_lib::Router;
+use std::borrow::{Borrow, BorrowMut};
 
 // aka stylus_sdk
 mod stylus_lib {
@@ -12,17 +13,31 @@ mod stylus_lib {
 
 // aka rust_contracts_stylus
 mod oz_lib {
+    use std::borrow::BorrowMut;
     use std::collections::HashMap;
     use std::fmt::Debug;
     use std::hash::Hash;
 
-    pub trait IErc721 {
-        fn transfer(&mut self);
+    // iterface
+    pub trait IErc721: VErc721 {
+        fn transfer(&mut self) {
+            println!("call base transfer");
+            self.update()
+        }
 
-        fn __route(&mut self, selector: u32, input: Vec<u8>) -> Option<()> {
+        fn mint(&mut self) {
+            println!("call base mint");
+            self.update()
+        }
+
+        fn __route(&mut self, selector: u32, input: &Vec<u8>) -> Option<()> {
             match selector {
                 0 => {
                     self.transfer();
+                    Some(())
+                }
+                1 => {
+                    self.mint();
                     Some(())
                 }
                 _ => None,
@@ -35,23 +50,44 @@ mod oz_lib {
         pub balances: HashMap<u32, u32>,
     }
 
-    impl IErc721 for Erc721 {
-        fn transfer(&mut self) {
-            println!("call public transfer");
+    impl VErc721 for Erc721 {
+        type Base = Self;
+
+        fn update(&mut self) {
+            println!("call base update");
+        }
+
+        fn owner_of(&mut self) {
+            println!("call base owner_of");
         }
     }
 
+    impl IErc721 for Erc721 {}
+
+    pub trait VErc721: BorrowMut<Self::Base> {
+        type Base: VErc721;
+
+        fn update(&mut self) {
+            self.borrow_mut().update()
+        }
+
+        fn owner_of(&mut self) {
+            self.borrow_mut().owner_of()
+        }
+    }
+
+    // inteface
     pub trait IPausable {
         fn pause(&mut self);
         fn unpause(&mut self);
 
-        fn __route(&mut self, selector: u32, input: Vec<u8>) -> Option<()> {
+        fn __route(&mut self, selector: u32, input: &Vec<u8>) -> Option<()> {
             match selector {
-                0 => {
+                2 => {
                     self.pause();
                     Some(())
                 }
-                1 => {
+                3 => {
                     self.unpause();
                     Some(())
                 }
@@ -65,37 +101,21 @@ mod oz_lib {
         paused: bool,
     }
 
-    impl IPausable for Pausable {
-        fn pause(&mut self) {
-            println!("call public pause");
-        }
-
-        fn unpause(&mut self) {
-            println!("call public unpause");
+    impl Pausable {
+        pub fn when_paused(&mut self) {
+            println!("call when_paused - paused: {}", self.paused);
         }
     }
 
-    // pub trait VErc721: SErc721 {
-    //     fn mint(&mut self) {
-    //         println!("call virtual mint");
-    //         self.update();
-    //     }
-    //
-    //     fn update(&mut self) {
-    //         println!("call virtual update");
-    //         self.owner_of();
-    //         self.get_mut().balances.insert(1, 1);
-    //     }
-    //
-    //     fn owner_of(&self) {
-    //         println!("call virtual owner_of");
-    //     }
-    // }
-    //
-    // pub trait SErc721 {
-    //     fn get(&self) -> &Erc721;
-    //     fn get_mut(&mut self) -> &mut Erc721;
-    // }
+    impl IPausable for Pausable {
+        fn pause(&mut self) {
+            println!("call pause");
+        }
+
+        fn unpause(&mut self) {
+            println!("call unpause");
+        }
+    }
 }
 
 // Client code
@@ -105,13 +125,36 @@ struct Erc721Example {
     pausable: Pausable,
 }
 
+impl Borrow<Erc721> for Erc721Example {
+    fn borrow(&self) -> &Erc721 {
+        &self.base
+    }
+}
+
+impl BorrowMut<Erc721> for Erc721Example {
+    fn borrow_mut(&mut self) -> &mut Erc721 {
+        &mut self.base
+    }
+}
+
+impl VErc721 for Erc721Example {
+    type Base = Erc721;
+
+    fn update(&mut self) {
+        self.pausable.when_paused();
+        self.base.update();
+    }
+}
+
+impl IErc721 for Erc721Example {}
+
 impl Router for Erc721Example {
     fn route(&mut self, selector: u32, input: Vec<u8>) -> Option<()> {
-        if let Some(()) = self.base.__route(selector, input) {
+        if let Some(()) = self.base.__route(selector, &input) {
             return Some(());
         }
 
-        if let Some(()) = self.pausable.__route(selector, input) {
+        if let Some(()) = self.pausable.__route(selector, &input) {
             return Some(());
         }
 
@@ -121,5 +164,5 @@ impl Router for Erc721Example {
 
 fn main() {
     let mut erc721 = Erc721Example::default();
-    erc721.route(0, vec![]).unwrap()
+    erc721.transfer()
 }
